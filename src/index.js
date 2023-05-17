52051e1ddcf7804305053c55a47cb157492dcfd3
Illia Shatokhin <resonane.2106@gmail.com>
2023-05-17 14:48:31 +0300

    summary

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const { default: axios } = require('axios');
+
+// Notify.success('Sol lucet omnibus');
+// Notify.failure('Qui timide rogat docet negare');
+// Notify.warning('Memento te hominem esse');
+// Notify.info('Cogito ergo sum');
